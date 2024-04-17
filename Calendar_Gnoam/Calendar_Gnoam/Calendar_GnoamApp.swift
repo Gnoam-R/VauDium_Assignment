c811599a54a42dab1d393026c6e3f8d4bcd6ff5d
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-04-17 09:58:09 +0900

    feat: Calender 프로젝트 생성 및 gitignore 추가

---
+++ Calendar_Gnoam/Calendar_Gnoam/Calendar_GnoamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_GnoamApp.swift
+//  Calendar_Gnoam
+//
+//  Created by Roh on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_GnoamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
