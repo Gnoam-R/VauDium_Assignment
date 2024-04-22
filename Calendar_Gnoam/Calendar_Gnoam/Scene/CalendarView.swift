2f514c2e234ede14f79f1326123a98fc011d0fb1
Gnoam-R <67363759+rohhyungwoo@users.noreply.github.com>
2024-04-22 17:19:40 +0900

    feat: CalendarView 적용

--- Calendar_Gnoam/Calendar_Gnoam/Scene/CalendarView.swift
+++ Calendar_Gnoam/Calendar_Gnoam/Scene/CalendarView.swift
@@ -11,15 +11,79 @@ struct CalendarView: View {
     @State var month: Date = Date()
     @State var offset: CGSize = CGSize()
     
+    @StateObject var calendarModel = CalendarModel()
+    @State var monthId: MonthModel.ID?
+    @State var showMonthLabel: Bool = false
+    @State var viewInitialized: Bool = false
+    
     var body: some View {
         
-        VStack {
+        VStack(
+            alignment: .center,
+            spacing: 0
+        ) {
             headerView
-            ScrollView(.vertical) {
-                LazyVStack {
-//                    ForEach(calendarModel.)
+            // monthly calendar stack
+            ZStack(
+                alignment: .top
+            ) {
+                // month label
+                if (showMonthLabel) {
+                    let displayMonthModel = calendarModel.monthModelFromId(monthId)
+                    
+                    Text(displayMonthModel.firstDayOfMonth.yearMonthString())
+                        .font(.system(size: 20))
+                        .fontWeight(Font.Weight.bold)
+                        .foregroundStyle(Color.black)
+                        .foregroundStyle(Color.red)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .transition(.opacity)
+                        .padding(.vertical, 5)
+                        .background(Color.white)
+                        .zIndex(2.0)
                 }
+                
+                // scrollable monthly view
+                ScrollView {
+                    LazyVStack {
+                        ForEach(calendarModel.monthList) { monthModel in
+                            MonthView(monthModel: monthModel)
+                                .onAppear {
+                                    if monthModel == calendarModel.monthList.last{
+                                        Task {
+                                            await calendarModel.addMonthAfter(5)
+                                        }
+                                    } else if monthModel ==  calendarModel.monthList.first {
+                                        Task {
+                                            await calendarModel.addMonthBefore(5)
+                                        }
+                                    }
+                                }
+                        }
+                    }
+                    .scrollTargetLayout()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                }
+                .scrollPosition(id: $monthId)
+                .padding(.vertical, 10)
+                .onAppear{ monthId = calendarModel.idForCurrentMonth() }
+                .scrollIndicators(.hidden)
+                .onChange(of: monthId, initial: false) {
+                    if ( !viewInitialized ) {
+                        viewInitialized = true
+                        return
+                    }
+                    showMonthLabel = true
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation(.smooth) {
+                            showMonthLabel = false
+                        }
+                    }
+                }
+
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+
         }
         .gesture(
             DragGesture()
